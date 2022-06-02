e06111f5c6f75b543b9e368068f3671be675583d
abdulsamad1000 <abdulsamad10000@yahoo.com>
2022-06-01 20:33:09 -0500

    modified the git class04

--- src/com/syntax/NewClass04/gitClass04.java
+++ src/com/syntax/NewClass04/gitClass04.java
@@ -3,6 +3,7 @@ package com.syntax.NewClass04;
 public class gitClass04 {
 	public static void main(String[] args) {
 		System.out.println("popculture");
+		System.out.println("abdrdkekro");
 	}
 
 }
